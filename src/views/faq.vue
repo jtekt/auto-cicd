a2b8e29734c5292e8410872eba418da79266e104
Kevin Fabel <kevin_fabel@jtekt.co.jp>
2025-02-25 13:47:37 +0900

    Improve faq page

--- src/views/faq.vue
+++ src/views/faq.vue
@@ -9,45 +9,46 @@
           GitLab</v-expansion-panel-title
         >
         <v-expansion-panel-text>
-          <ol>
-            <li>
-              <strong>Log in to GitLab:</strong> Open your GitLab instance and
-              log in to your account.
-            </li>
-            <li>
-              <strong>Navigate to the Project:</strong> Go to the project you
+          <v-list lines="one">
+            <v-list-item>
+              <strong>1. Log in to GitLab:</strong> Open your GitLab instance
+              and log in to your account.
+            </v-list-item>
+            <v-list-item>
+              <strong>2. Navigate to the Project:</strong> Go to the project you
               want to move.
-            </li>
-            <li>
-              <strong>Go to Project Settings:</strong> In the left sidebar of
+            </v-list-item>
+            <v-list-item>
+              <strong>3. Go to Project Settings:</strong> In the left sidebar of
               your project, click on <strong>Settings</strong>, then
               <strong>General</strong> to open the general settings of the
               project.
-            </li>
-            <li>
-              <strong>Expand the "Advanced" Section:</strong> Scroll down to
+            </v-list-item>
+            <v-list-item>
+              <strong>4. Expand the "Advanced" Section:</strong> Scroll down to
               find the <strong>Advanced</strong> section, and click on it to
               expand the options.
-            </li>
-            <li>
-              <strong>Change Namespace:</strong> In the
+            </v-list-item>
+            <v-list-item>
+              <strong>5. Change Namespace:</strong> In the
               <strong>Advanced</strong> section, you should see a field called
               <strong>"Transfer project"</strong>. Click on the
               <strong>"Transfer project"</strong> button.
-            </li>
-            <li>
-              <strong>Choose the New Namespace:</strong> A dialog will pop up
+            </v-list-item>
+            <v-list-item>
+              <strong>6. Choose the New Namespace:</strong> A dialog will pop up
               asking you to select the <strong>target namespace</strong>. You
               can select either a <strong>Group</strong> or another
               <strong>user namespace</strong>. Select the appropriate namespace
-              and click <strong>Transfer project</strong>.
-            </li>
-            <li>
-              <strong>Confirmation:</strong> GitLab will ask you to confirm the
-              move. Once confirmed, the repository will be transferred to the
-              new namespace.
-            </li>
-          </ol>
+              (auto-cicd or a subgroup) and click
+              <strong>Transfer project</strong>.
+            </v-list-item>
+            <v-list-item>
+              <strong>7. Confirmation:</strong> GitLab will ask you to confirm
+              the move. Once confirmed, the repository will be transferred to
+              the new namespace.
+            </v-list-item>
+          </v-list>
         </v-expansion-panel-text>
       </v-expansion-panel>
     </v-expansion-panels>
