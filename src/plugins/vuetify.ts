03f3f9ba3de1a106926ba85970d485989219b121
Kevin Fabel <kevin_fabel@jtekt.co.jp>
2025-02-27 16:10:43 +0900

    Set base language

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -150,12 +150,12 @@ const messages = {
 };
 
 // Get the saved language from localStorage, defaulting to 'en'
-const savedLanguage = localStorage.getItem("preferred_language") || "ja";
+const savedLanguage = localStorage.getItem("preferred_language") || "en";
 
 const i18n = createI18n({
   legacy: false, // Vuetify does not support the legacy mode of vue-i18n
   locale: savedLanguage,
-  fallbackLocale: "en",
+  fallbackLocale: "ja",
   messages,
 });
 
