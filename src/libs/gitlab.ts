dfccfaf0fe2808a1e19c676e3729628b838dfc98
Kevin Fabel <kevin_fabel@jtekt.co.jp>
2025-04-16 11:54:07 +0900

    Update group prefix

--- src/libs/gitlab.ts
+++ src/libs/gitlab.ts
@@ -36,7 +36,7 @@ export const UserSchema = z.object({
 
     // Prefix with "ci-" if it starts with a number
     if (/^[0-9]/.test(transformed)) {
-      transformed = `ci-${transformed}`;
+      transformed = `autocicd-${transformed}`;
     }
 
     return transformed;
